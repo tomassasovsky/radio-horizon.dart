f1e78bdfeddaac92fb7f2951ffdc2b1c2fe57e48
Tomás Sasovsky <to.sasovsky@gmail.com>
2023-03-22 14:21:19 +0100

    fix: bad config in relaunch_prod.sh

--- relaunch_prod.bat
+++ relaunch_prod.bat
@@ -4,7 +4,7 @@ CALL docker-compose stop radio_horizon
 
 @REM remove images
 CALL docker image rm fredboat/lavalink:master -f
-CALL docker image rm radio-horizondart-radio_horizon:latest -f
+CALL docker image rm ghcr.io/tomassasovsky/radio-horizon.dart:latest -f
 
 @REM remove containers
 CALL docker rm lavalink -f
